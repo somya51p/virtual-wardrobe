dc8e2b3ef4206857258a38b4dd133fbc5ef32bd3
somya51p <somya2001p@gmail.com>
2021-11-14 22:00:01 +0530

    wardrobe improvised

--- src/components/wardrobe.jsx
+++ src/components/wardrobe.jsx
@@ -1,37 +1,64 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import c from "../inputs/c.jpg";
-import p from "../inputs/p.jpg";
-import o from "../output/output.png";
+import cloth1 from "../inputs/c.jpg";
+import person1 from "../inputs/p.jpg";
+import person2 from "../inputs/example_person.jpg";
+import person3 from "../inputs/test-model2.jpg";
+import person4 from "../inputs/model_5.jpg";
+import output from "../output/output.png";
 import "./style.css";
 
 export const Wardrobe = () => {
   const [load, setLoad] = useState(0);
-  console.log("somyaa");
+  const [person, setPerson] = useState("inputs/p.jpg");
+  const [cloth, setCloth] = useState("inputs/c.jpg");
+   
   const handleTry = async () => {
     setLoad(1);
-    const res = await axios.post("/api", {person: "inputs/p.jpg", cloth: "inputs/c.jpg"});
+    console.log(person,cloth);
+    const res = await axios.post("/api", {person, cloth});
     console.log(res);
     setLoad(2);
   }
 
   return (
     
-    <div id="#head" className="base-container">
-      Wardrobe!!!
+    <div id="#head" className="inset">
+      <h1>Virtual Wardrobe!!!</h1>
+  
       Cloth:
-      <img src={c} alt="cloth"/>
+      <img src={cloth1} alt="cloth"/>
+
       Person:
-      <img src={p} alt="person"/>
+      <img src={person1} alt="person"/>
+      <button onClick={()=>setPerson(`input/c.jpg`)}>
+        <span>Choose</span>
+      </button>
+      Person:
+      <img src={person2} alt="person"/>
+      <button onClick={()=>setPerson(`input/example_person.jpg`)}>
+        <span>Choose</span>
+      </button>
+      Person:
+      <img src={person3} alt="person"/>
+      <button onClick={()=>setPerson(`input/test-model2.jpg`)}>
+        <span>Choose</span>
+      </button>
+      Person:
+      <img src={person4} alt="person"/>
+      <button onClick={()=>setPerson(`input/model_5.jpg`)}>
+        <span>Choose</span>
+      </button>
+
       <button onClick={handleTry}>
-        <span>Try On</span>
+        <span>Try-On</span>
       </button>
       {
         load===1 && <h3>Loading...</h3>
       }
       {
-        load===2 && <img src={o} alt="output"/>
+        load===2 && <img src={output} alt="output"/>
       }
     </div>
   );
